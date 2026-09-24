da866b746cce92e5a3c1d18b6cb9f5e64e807ccd
Sahel <Sahel.Naghshineh@gmail.com>
2026-09-25 00:17:54 +0200

    Add ReverseBinary solution

---
+++ ReverseBinary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseBinary
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input)) return;
+
+            int number = int.Parse(input);
+            Console.WriteLine(ReverseBits(number));
+        }
+        static int ReverseBits(int number)
+        {
+            var binary = Convert.ToString(number, 2);
+            char[] chars = binary.ToCharArray();
+            Array.Reverse(chars);
+            var reversedBinary = new string(chars);
+            return Convert.ToInt32(reversedBinary, 2);
+        }
+    }
+}
